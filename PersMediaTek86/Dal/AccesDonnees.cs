39f7b1c6fa88b3b7b0b0a0420a6d384f24d44046
ljenniferl <jennifer.landy.pro@gmail.com>
2022-05-17 10:48:37 +0200

    Package Dal, classe ConnexionBDD. Chaine de connexion à la BDD.

---
+++ PersMediaTek86/Dal/AccesDonnees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersMediaTek86.Dal
+{
+    /// <summary>
+    /// Classe permettant de gérer les demandes concernant les données distantes
+    /// </summary>
+    public class AccesDonnees
+    {
+        /// <summary>
+        /// Chaine de connexion à la BDD
+        /// </summary>
+        private static string connectionString = "server=localhost;user id=userDB;password=userDB;database=PersMediaTek86;SslMode=none";
+    }
+
+
+}
